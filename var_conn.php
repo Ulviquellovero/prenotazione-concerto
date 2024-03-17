1b92a2b0d0ea807b4eb3917dfebb79b5aab79472
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-03-17 11:19:41 +0100

    x scuola

--- var_conn.php
+++ var_conn.php
@@ -1,4 +1,3 @@
 <?php
-    //$con = mysqli_connect("localhost","quintaf","Qu!nta","pren_ulivi");
-    $con = mysqli_connect("localhost","root","","pren_ulivi");
+    $con = mysqli_connect("localhost","quintaf","Qu!nta","pren_ulivi");
 ?>
