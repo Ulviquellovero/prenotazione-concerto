eebda522d4b5c0c64466b12b290f493582ff0eb0
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-03-20 11:52:18 +0100

    Aggiungere json prenota

--- prenota.php
+++ prenota.php
@@ -1,9 +1,15 @@
 <?php
 	require_once("var_conn.php");
-    if(isset($_REQUEST['idPosto']))
+    $idSessione = session_id();
+    $sql = "SELECT * FROM tprenotazione WHERE idSessioneUtente = '$idSessione'";
+    $res = mysqli_query($con, $sql);
+    if(mysqli_num_rows($res) == 0)
     {
-        $idPosto = $_REQUEST['idPosto'];
-        $sql = "UPDATE tprenotazione SET prenotato = 1 WHERE id = $idPosto";
-        mysqli_query($con,$sql);
+        if(isset($_REQUEST['idPosto']))
+        {
+            $idPosto = $_REQUEST['idPosto'];
+            $sql = "UPDATE tprenotazione SET prenotato = 1, idSessioneUtente = '$idSessione' WHERE id = $idPosto";
+            mysqli_query($con,$sql);
+        }
     }
 ?>
