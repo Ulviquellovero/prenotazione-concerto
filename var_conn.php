3f8795ecb407409d542b5533addc6ad939ef8a3b
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-03-22 08:06:25 +0100

    Var Conn sistemate

--- var_conn.php
+++ var_conn.php
@@ -1,5 +1,4 @@
 <?php
     session_start();
-    //$con = mysqli_connect("localhost","quintaf","Qu!nta","pren_ulivi");
-    $con = mysqli_connect("localhost","root","","pren_ulivi");
+    $con = mysqli_connect("localhost","quintaf","Qu!nta","pren_ulivi");
 ?>
