860572b74169691b043eddb4bb13e8977e669eef
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-03-22 08:05:26 +0100

    Finito

--- prenota.php
+++ prenota.php
@@ -3,6 +3,7 @@
     $idSessione = session_id();
     $sql = "SELECT * FROM tprenotazione WHERE idSessioneUtente = '$idSessione'";
     $res = mysqli_query($con, $sql);
+    $prenotato = false;
     if(mysqli_num_rows($res) == 0)
     {
         if(isset($_REQUEST['idPosto']))
@@ -10,6 +11,11 @@
             $idPosto = $_REQUEST['idPosto'];
             $sql = "UPDATE tprenotazione SET prenotato = 1, idSessioneUtente = '$idSessione' WHERE id = $idPosto";
             mysqli_query($con,$sql);
+            $prenotato = true;
         }
     }
+    $res = array(
+        'prenotato' => $prenotato
+    );
+    echo json_encode($res);
 ?>
