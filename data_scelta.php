c1e764fe54b2c3484b7ffa332d21e9fe4a6d96cf
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-04-19 15:41:53 +0200

    Login Funzionante

--- data_scelta.php
+++ data_scelta.php
@@ -1,5 +1,7 @@
 <?php
-    session_start();
+    require_once("var_conn.php");
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: *");
     if(isset($_GET["idData"]))
         $_SESSION["idData"] = $_GET["idData"];
     header("Location: lista_biglietti.html");
